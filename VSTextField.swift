7270f562f8f47528896d9d77d2796e012add01f0
PhongsakornP <i.phongsakornp@gmail.com>
2015-12-12 17:23:01 +0700

    Update to Swift 2.0

--- VSTextField.swift
+++ VSTextField.swift
@@ -3,6 +3,10 @@
 //
 //  Created by Vojta Stavik on 09/03/15.
 //  Copyright (c) 2015 www.STRV.com All rights reserved.
+//  https://github.com/VojtaStavik/VSTextField
+//  http://vojtastavik.com/2015/03/29/real-time-formatting-in-uitextfield-swift-basics/
+//
+//  Modified by PhongsakornP.
 //
 
 import UIKit
@@ -16,52 +20,46 @@ enum TextFieldFormatting {
 
 
 class VSTextField: UITextField {
-
+    
     /**
-    Set a formatting pattern for a number and define a replacement string. For example: If formattingPattern would be "##-##-AB-##" and
-    replacement string would be "#" and user input would be "123456", final string would look like "12-34-AB-56"
-    */
+     Set a formatting pattern for a number and define a replacement string. For example: If formattingPattern would be "##-##-AB-##" and
+     replacement string would be "#" and user input would be "123456", final string would look like "12-34-AB-56"
+     */
     func setFormatting(formattingPattern: String, replacementChar: Character) {
-        
         self.formattingPattern = formattingPattern
         self.replacementChar = replacementChar
     }
     
     
     /**
-    A character which will be replaced in formattingPattern by a number
-    */
+     A character which will be replaced in formattingPattern by a number
+     */
     var replacementChar: Character = "*"
-
+    
     
     /**
-    A character which will be replaced in formattingPattern by a number
-    */
+     A character which will be replaced in formattingPattern by a number
+     */
     var secureTextReplacementChar: Character = "\u{25cf}"
     
     
     /**
-    Max length of input string. You don't have to set this if you set formattingPattern.
-    If 0 -> no limit.
-    */
+     Max length of input string. You don't have to set this if you set formattingPattern.
+     If 0 -> no limit.
+     */
     var maxLength = 0
     
     
     /**
-    Type of predefined text formatting. (You don't have to set this. It's more a future feature)
-    */
+     Type of predefined text formatting. (You don't have to set this. It's more a future feature)
+     */
     var formatting : TextFieldFormatting = .NoFormatting {
         
         didSet {
-            
             if formatting == .SocialSecurityNumber {
-                
                 self.formattingPattern = "***-**–****"
                 self.replacementChar = "*"
-            }
-            
-            else {
-                
+            } else {
                 self.maxLength = 0
             }
         }
@@ -69,71 +67,63 @@ class VSTextField: UITextField {
     
     
     /**
-    String with formatting pattern for the text field.
-    */
+     String with formatting pattern for the text field.
+     */
     var formattingPattern: String = "" {
         
         didSet {
-            
-            self.maxLength = count(formattingPattern)
+
+            self.maxLength = formattingPattern.characters.count
             self.formatting = .Custom
         }
     }
-
+    
     
     /**
-    Provides secure text entry but KEEPS formatting. All digits are replaced with the bullet character \u{25cf} .
-    */
+     Provides secure text entry but KEEPS formatting. All digits are replaced with the bullet character \u{25cf} .
+     */
     var formatedSecureTextEntry: Bool {
         
         set {
-            
             _formatedSecureTextEntry = newValue
             super.secureTextEntry = false
         }
         
         get {
-            
             return _formatedSecureTextEntry
         }
     }
     
     
     override var text: String! {
-        
         set {
-            
             super.text = newValue
             textDidChange() // format string properly even when it's set programatically
         }
         
         get {
-            
             return formatting == .NoFormatting ? super.text : finalStringWithoutFormatting
         }
     }
     
     
     required init(coder aDecoder: NSCoder) {
-        
-        super.init(coder: aDecoder)
+        super.init(coder: aDecoder)!
         registerForNotifications()
     }
     
     override init(frame: CGRect) {
-        
         super.init(frame: frame)
         registerForNotifications()
     }
     
     deinit {
-        
         NSNotificationCenter.defaultCenter().removeObserver(self)
     }
     
     /**
-    Final text without formatting characters (read-only)
-    */
+     Final text without formatting characters (read-only)
+     */
     var finalStringWithoutFormatting : String {
         
         return VSTextField.makeOnlyDigitsString(_textWithoutSecureBullets)
@@ -143,8 +133,8 @@ class VSTextField: UITextField {
     // MARK: - class methods
     
     class func makeOnlyDigitsString(string: String) -> String {
-        
-        return join("", string.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet))
+
+        return string.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet).joinWithSeparator("")
     }
     
     
@@ -169,84 +159,77 @@ class VSTextField: UITextField {
         // TODO: - Isn't there more elegant way how to do this?
         let currentTextForFormatting: String
         
-        if count(super.text) > count(_textWithoutSecureBullets) {
-            
-            currentTextForFormatting = _textWithoutSecureBullets + super.text.substringFromIndex(advance(super.text.startIndex, count(_textWithoutSecureBullets)))
-        }
-        
-        else if count(super.text) == 0 {
-         
-            _textWithoutSecureBullets = ""
-            currentTextForFormatting = ""
-        }
-        
-        else {
-            
-            currentTextForFormatting = _textWithoutSecureBullets.substringToIndex(advance(_textWithoutSecureBullets.startIndex, count(super.text)))
-        }
-        
-        
-        if formatting != .NoFormatting && count(currentTextForFormatting) > 0 && count(formattingPattern) > 0 {
-            
-            let tempString = VSTextField.makeOnlyDigitsString(currentTextForFormatting)
-            
-            var finalText = ""
-            var finalSecureText = ""
-            
-            var stop = false
-            
-            var formatterIndex = formattingPattern.startIndex
-            var tempIndex = tempString.startIndex
-            
-            while !stop {
+        if let superText = super.text {
+            if superText.characters.count > _textWithoutSecureBullets.characters.count {
+                currentTextForFormatting = _textWithoutSecureBullets + superText.substringFromIndex(superText.startIndex.advancedBy(_textWithoutSecureBullets.characters.count))
+            }
                 
-                let formattingPatternRange = Range(start: formatterIndex, end: advance(formatterIndex, 1) )
+            else if superText.characters.count == 0 {
                 
-                if formattingPattern.substringWithRange(formattingPatternRange) != String(replacementChar) {
-                    
-                    finalText = finalText.stringByAppendingString(formattingPattern.substringWithRange(formattingPatternRange))
-                    finalSecureText = finalSecureText.stringByAppendingString(formattingPattern.substringWithRange(formattingPatternRange))
-                }
-                    
-                else if count(tempString) > 0 {
-                    
-                    let pureStringRange = Range(start: tempIndex, end: advance(tempIndex, 1))
-                    
-                    finalText = finalText.stringByAppendingString(tempString.substringWithRange(pureStringRange))
-                    
-                    // we want the last number to be visible
-                    if advance(tempIndex, 1) == tempString.endIndex {
-                        
-                        finalSecureText = finalSecureText.stringByAppendingString(tempString.substringWithRange(pureStringRange))
-                    }
-                    
-                    else {
-                        
-                        finalSecureText = finalSecureText.stringByAppendingString(String(secureTextReplacementChar))
-                    }
-                    
-                    tempIndex++
-                }
+                _textWithoutSecureBullets = ""
+                currentTextForFormatting = ""
+            }
                 
-                formatterIndex++
-                
-                if formatterIndex >= formattingPattern.endIndex || tempIndex >= tempString.endIndex {
-                    
-                    stop = true
-                }
+            else {
+                currentTextForFormatting = _textWithoutSecureBullets.substringToIndex(_textWithoutSecureBullets.startIndex.advancedBy(superText.characters.count))
             }
             
-            _textWithoutSecureBullets = finalText
-            super.text = _formatedSecureTextEntry ? finalSecureText : finalText
-        }
-        
-        // Let's check if we have additional max length restrictions
-        if maxLength > 0 {
             
-            if count(text) > maxLength {
+            if formatting != .NoFormatting && currentTextForFormatting.characters.count > 0 && formattingPattern.characters.count > 0 {
                 
-                super.text = text.substringToIndex(advance(text.startIndex, maxLength))
-                _textWithoutSecureBullets = _textWithoutSecureBullets.substringToIndex(advance(_textWithoutSecureBullets.startIndex, maxLength))
+                let tempString = VSTextField.makeOnlyDigitsString(currentTextForFormatting)
+                
+                var finalText = ""
+                var finalSecureText = ""
+                
+                var stop = false
+                
+                var formatterIndex = formattingPattern.startIndex
+                var tempIndex = tempString.startIndex
+                
+                while !stop {
+                    let formattingPatternRange = Range(start: formatterIndex, end: formatterIndex.advancedBy(1) )
+                    
+                    if formattingPattern.substringWithRange(formattingPatternRange) != String(replacementChar) {
+                        finalText = finalText.stringByAppendingString(formattingPattern.substringWithRange(formattingPatternRange))
+                        finalSecureText = finalSecureText.stringByAppendingString(formattingPattern.substringWithRange(formattingPatternRange))
+                    }
+                        
+                    else if tempString.characters.count > 0 {
+                        let pureStringRange = Range(start: tempIndex, end: tempIndex.advancedBy(1))
+                        
+                        finalText = finalText.stringByAppendingString(tempString.substringWithRange(pureStringRange))
+                        
+                        // we want the last number to be visible
+                        if tempIndex.advancedBy(1) == tempString.endIndex {
+                            finalSecureText = finalSecureText.stringByAppendingString(tempString.substringWithRange(pureStringRange))
+                        } else {
+                            finalSecureText = finalSecureText.stringByAppendingString(String(secureTextReplacementChar))
+                        }
+                        
+                        tempIndex++
+                    }
+                    
+                    formatterIndex++
+                    
+                    if formatterIndex >= formattingPattern.endIndex || tempIndex >= tempString.endIndex {
+                        stop = true
+                    }
+                }
+                
+                _textWithoutSecureBullets = finalText
+                super.text = _formatedSecureTextEntry ? finalSecureText : finalText
+            }
+            
+            // Let's check if we have additional max length restrictions
+            if maxLength > 0 {
+                
+                if text.characters.count > maxLength {
+                    
+                    super.text = text.substringToIndex(text.startIndex.advancedBy(maxLength))
+
+                    _textWithoutSecureBullets = _textWithoutSecureBullets.substringToIndex(_textWithoutSecureBullets.startIndex.advancedBy(maxLength))
+                }
             }
         }
     }
